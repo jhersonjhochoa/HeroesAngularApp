f3eb8ef5618a9634e61d600d6cb26f944c044443
jhersonjhochoa <jhersonjhochoa@gmail.com>
2020-01-31 22:04:35 -0500

    corrección buscador

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
-import { Router } from '@angular/router'
+// import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-heroes',
@@ -10,18 +10,16 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService:HeroesService, private _router:Router) { 
+  // private _router:Router) { 
+  constructor( private _heroesService:HeroesService) { 
     // console.log("constructor");
   }
 
   ngOnInit() {
-    // console.log("onInit");
     this.heroes = this._heroesService.getHeroes();
-    // console.log( this.heroes );
-
   }
 
-  verHeroe(idx:number){
-    this._router.navigate( ['/heroe', idx] )
-  }
+  // verHeroe(idx:number){
+  //   this._router.navigate( ['/heroe', idx] )
+  // }
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -70,9 +70,12 @@ export class HeroesService {
     buscarHeroes(termino:string){
       let heroesArr:Heroe[] = [];
       termino = termino.toLowerCase();
-      for ( let heroe of this.heroes ){
+      // for ( let heroe of this.heroes ){
+      for (let i = 0; i < this.heroes.length; i++){
+        let heroe = this.heroes[i];
         let nombre = heroe.nombre.toLowerCase();
         if (nombre.indexOf(termino) >= 0){
+          heroe.idx = i;
           heroesArr.push( heroe )
         }
       }
@@ -88,4 +91,5 @@ export interface Heroe{
     img: string;
     aparicion: string;
     casa: string;
+    idx?: number;
 }
